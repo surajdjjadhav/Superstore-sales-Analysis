15fd9c35e237554b765a1c25be506120248f7422
surajdjjadhav <sj4456046@gmail.com>
2024-12-30 18:56:12 +0530

    suraj

---
+++ data analysist.sql
@@ -0,0 +1,20 @@
+
+#  Question 1 : Calculate the average movie rating for each genre.
+
+WITH AvgRatings AS (
+    SELECT movieId, AVG(rating) AS avg_rating
+    FROM rating 
+    GROUP BY movieId
+)
+SELECT g.genres, AVG(ar.avg_rating) AS avg_genre_rating
+FROM AvgRatings ar
+JOIN movie g ON ar.movieId = g.movieId
+GROUP BY g.genres
+order by avg_genre_rating desc;
+
+
+
+
+
+
+
